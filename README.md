4030dfe0efafccb723941c792be7732016861432
Herlon Aguiar <herlon214@gmail.com>
2017-02-22 15:07:05 -0400

    Release versão 2.0.0

--- README.md
+++ README.md
@@ -17,13 +17,25 @@ Webextension com melhorias no [SIGECAD-Acadêmico da UFGD](http://sigecad-academ
 ![Imagem do botão na página de Estrutura Curricular](./screen_feature1.png)
 
 
+### Contribua
+
+Para contribuir é necessário seguir algumas convenções:
+
+* O idioma padrão deve ser o pt-BR. Tanto nos commits quanto no nome das variáveis etc. A extensão é voltada ao público brasileiro, é bom definir esse idioma padrão pois pode facilitar o entendimento de quem está começando a programar e quer ajudar.
+* Variáveis com padrão lowerCamelCase. ([clique aqui para saber mais sobre](https://pt.wikipedia.org/wiki/CamelCase))
+* Quando uma alteração for feita é necessário mudar a versão da extensão no manifest.json. Estamos seguindo o padrão SEMVER. ([clique aqui para saber mais sobre](http://semver.org/lang/pt-BR/))
+
 ### Changelog
 
+* 2.0.0
+	* Removido ZeptoJS, FetchPollyfill e PromisePollyfill para poder ser aceito pela Mozilla.
+	* Inserido o jQuery
+
 * 1.1.3
-	*[FEATURE] Adicionado complementos das legendas de acordo com regra da UFGD.
+	* [FEATURE] Adicionado complementos das legendas de acordo com regra da UFGD.
 
 * 1.1.2
-	*[FEATURE] Adicionado complementos da legenda de APE aprovado por exame
+	* [FEATURE] Adicionado complementos da legenda de APE aprovado por exame
 
 * 1.1.1
   * [FEATURE] Mostrar como aprovado as matérias que foram dispensadas por currículo (dica do Filipe De Andrade Maldonado e Adolfo Eduardo)
--- extrairMaterias.js
+++ extrairMaterias.js
@@ -1,97 +1,96 @@
-Zepto(function($) {
-var extrairInfo = function($tr) {
-  var conteudoPrincipal = $tr.find('td').eq(1).text();
+$(document).ready(function() {
+  var extrairInfo = function($tr) {
+    var conteudoPrincipal = $tr.find('td').eq(1).text();
 
-  if(conteudoPrincipal) {
-    var codigo = conteudoPrincipal.split(' - ')[0];
-    var nome = conteudoPrincipal.split(' - ')[1];
-    var status = $tr.find('td').last().text();
+    if(conteudoPrincipal) {
+      var codigo = conteudoPrincipal.split(' - ')[0];
+      var nome = conteudoPrincipal.split(' - ')[1];
+      var status = $tr.find('td').last().text();
 
-    return {
-      codigo: codigo,
-      nome: nome.trim(), // Usa o trim para retirar os espaços em branco extra
-      status: status.trim() // Usa o trim para retirar os espaços em branco extra
-    }
-  }else {
-    return null
-  }
-
-}
-
-/**
- * Por que verificar se a matéria já existe?
- * R: A ordem das matérias no histórico é do mais recente para o mais velho.
- * Então, quando você reprova e depois passa na matéria (se ela não mudar de número)
- * vai aparecer como reprovado, pois é o item mais antigo no array
- */
-var materiaJaExiste = function(materias, materia) {
-  for(var i = 0; i < materias.length; i++) {
-    if(materias[i].codigo == materia.codigo) return true;
-  }
-
-  return false;
-}
-
-var pegarConteudoMaterias = function() {
-  return fetch('http://sigecad-academico.ufgd.edu.br/consultar/historico.jsf',{
-      credentials: 'include',
-      method: 'GET'
-    }).then(function(resp) {
-      return resp.text()
-    }).then(function(resp) {
-      return resp.split('<body>')[1]
-    })
-}
-
-var mostrarMateriasConcluidas = function() {
-  var materias = []
-
-  // Pega o html com o historico das matérias concluídas
-  pegarConteudoMaterias()
-  .then(function(html) {
-
-    $(html).find('.painel-conteudo table tr').forEach(function(item) {
-      var materia = extrairInfo($(item))
-      if(materia) {
-        // Verifica se a matéria já está no array
-        if(!materiaJaExiste(materias, materia)) {
-          materias.push(materia)
-        }
+      return {
+        codigo: codigo,
+        nome: nome.trim(), // Usa o trim para retirar os espaços em branco extra
+        status: status.trim() // Usa o trim para retirar os espaços em branco extra
       }
-    });
+    }else {
+      return null
+    }
 
-    // Marca as matérias já concluídas
-    materias.forEach(function(materia) {
-      $('td').forEach(function(item) {
-        if($(item).text() == materia.codigo) {
-          $(item).parent().find('td').forEach(function(subitem) {
+  }
 
-            $(subitem).removeClass('reprovado'); // Fix para as matérias que a pessoa reprovou mas depois passou
+  /**
+   * Por que verificar se a matéria já existe?
+   * R: A ordem das matérias no histórico é do mais recente para o mais velho.
+   * Então, quando você reprova e depois passa na matéria (se ela não mudar de número)
+   * vai aparecer como reprovado, pois é o item mais antigo no array
+   */
+  var materiaJaExiste = function(materias, materia) {
+    for(var i = 0; i < materias.length; i++) {
+      if(materias[i].codigo == materia.codigo) return true;
+    }
 
-            // Verifica o status da matéria
-            switch(materia.status) {
-              case "AP": $(subitem).addClass('aprovado'); break;
-              case "APE": $(subitem).addClass('aprovado'); break;
-              case "DS": $(subitem).addClass('aprovado'); break;
-              case "DC": $(subitem).addClass('aprovado'); break;
-              case "RP": $(subitem).addClass('reprovado'); break;
-              case "RF": $(subitem).addClass('reprovado'); break;
-              case "RN": $(subitem).addClass('reprovado'); break;
-              case "MA": $(subitem).addClass('matriculado'); break;
-            }
-          })
+    return false;
+  }
+
+  var pegarConteudoMaterias = function() {
+    return fetch('http://sigecad-academico.ufgd.edu.br/consultar/historico.jsf',{
+        credentials: 'include',
+        method: 'GET'
+      }).then(function(resp) {
+        return resp.text()
+      }).then(function(resp) {
+        return resp.split('<body>')[1]
+      })
+  }
+
+  var mostrarMateriasConcluidas = function() {
+    var materias = []
+
+    // Pega o html com o historico das matérias concluídas
+    pegarConteudoMaterias()
+    .then(function(html) {
+
+      $(html).find('.painel-conteudo table tr').each(function() {
+        var materia = extrairInfo($(this))
+        if(materia) {
+          // Verifica se a matéria já está no array
+          if(!materiaJaExiste(materias, materia)) {
+            materias.push(materia)
+          }
         }
+      });
+
+      // Marca as matérias já concluídas
+      materias.forEach(function(materia) {
+        $('td').each(function() {
+          if($(this).text() == materia.codigo) {
+            $(this).parent().find('td').each(function() {
+
+              $(this).removeClass('reprovado'); // Fix para as matérias que a pessoa reprovou mas depois passou
+
+              // Verifica o status da matéria
+              switch(materia.status) {
+                case "AP": $(this).addClass('aprovado'); break;
+                case "APE": $(this).addClass('aprovado'); break;
+                case "DS": $(this).addClass('aprovado'); break;
+                case "DC": $(this).addClass('aprovado'); break;
+                case "RP": $(this).addClass('reprovado'); break;
+                case "RF": $(this).addClass('reprovado'); break;
+                case "RN": $(this).addClass('reprovado'); break;
+                case "MA": $(this).addClass('matriculado'); break;
+              }
+            })
+          }
+        })
       })
     })
-  })
-  .catch(function(err) {
-    console.log(err)
-    alert('Falha ao pegar o conteúdo')
-  })
-}
+    .catch(function(err) {
+      console.log(err)
+      alert('Falha ao pegar o conteúdo')
+    })
+  }
 
   $('.mostrarMateriasConcluidas').remove();
   $('#formAcoes td').eq(0).append("<button type='button' class='btn btn-success mostrarMateriasConcluidas' title='UFGD Boost'>Comparar matérias</button>");
   $('.mostrarMateriasConcluidas').click(mostrarMateriasConcluidas);
-
 })
